e1491fc016d951eb011febdcb20e074554524522
Marat Kazbekov <marat@iMac-Marat.local>
2023-09-12 21:28:51 +0600

    add expressjs and one endpoint

--- app.js
+++ app.js
@@ -1,27 +1,22 @@
 require('dotenv/config');
 const axios = require('axios');
 
-const url = 'https://discord.com/api/v9/guilds/662267976984297473/messages/search';
+const url = process.env.SEARCH_URL;
 const headers = {
     'Authorization': process.env.AUTH_TOKEN
 };
 
-main();
-
-async function main() {
-    console.log(await getImages(0))
-}
-
-async function getImages(offset, content = 'red blood cells') {
+async function getImages(content, offset = 0) {
     const PAGE_LENGTH = 25;
-    return axios.get(url, { params: { offset, content }, headers })
+
+    return axios.get(url, { params: { content, offset }, headers })
         .then(response => {
             const urls = response.data.messages.map(m => m[0].attachments)
                 .filter(a => a[0])
                 .map(a => a[0].url)
                 .filter(url => url.endsWith('.png'));
 
-            console.log('Response:', response.data.total_results, urls);
+            console.log('Response:', response.data.total_results, urls[0]);
 
             if (urls.length > 0) {
                 return urls[0];
@@ -29,7 +24,7 @@ async function getImages(offset, content = 'red blood cells') {
             else {
                 if (offset < response.data.total_results) {
                     return new Promise(resolve => setTimeout(
-                            resolve(getImages(offset + PAGE_LENGTH))
+                            resolve(getImages(content, offset + PAGE_LENGTH))
                         , 1000));
                 }
                 else {
@@ -42,3 +37,4 @@ async function getImages(offset, content = 'red blood cells') {
         });
 }
 
+exports.getImages = getImages;
---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const images = require('./app.js')
+
+const app = express();
+const port = 3000;
+
+app.get('/images', async(req, res) => {
+    console.log(req.query)
+    const image = await images.getImages(req.query.content);
+    return res.json({ image });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
